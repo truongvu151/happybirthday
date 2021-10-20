723b565aadb16b175df0c2d74063a2b129c06741
truongvu151 <1951012151vu@ou.edu.vn>
2021-10-20 16:33:32 +0700

    Init Project

---
+++ music.js
@@ -0,0 +1,52 @@
+$(function()
+{
+    var playerTrack = $("#player-track");
+	var bgArtwork = $('#bg-artwork');
+	var bgArtworkUrl;
+	var albumName = $('#album-name');
+	var trackName = $('#track-name');
+	var albumArt = $('#album-art'),
+		sArea = $('#s-area'),
+		seekBar = $('#seek-bar'),
+		trackTime = $('#track-time'),
+		insTime = $('#ins-time'),
+		sHover = $('#s-hover'),
+		playPauseButton = $("#play-pause-button"),
+		i = playPauseButton.find('i'),
+		tProgress = $('#current-time'),
+		tTime = $('#track-length'),
+		seekT, seekLoc, seekBarPos, cM, ctMinutes, ctSeconds, curMinutes, curSeconds, durMinutes, durSeconds, playProgress, bTime, nTime = 0,
+		buffInterval = null, tFlag = false;
+	
+	var playPreviousTrackButton = $('#play-previous'), playNextTrackButton = $('#play-next'), currIndex = -1;
+	
+	var songs = [{
+		artist: "Dig Didzay",
+		name: "Nếu Anh Đi (Cover)",
+		url: "Musics/music.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	}];
+})
+function playPause()
+    {
+        setTimeout(function()
+        {
+            if(audio.paused)
+            {
+                playerTrack.addClass('active');
+                albumArt.addClass('active');
+                checkBuffering();
+                i.attr('class','fas fa-pause');
+                audio.play();
+            }
+            else
+            {
+                playerTrack.removeClass('active');
+                albumArt.removeClass('active');
+                clearInterval(buffInterval);
+                albumArt.removeClass('buffering');
+                i.attr('class','fas fa-play');
+                audio.pause();
+            }
+        },300);
+    }
